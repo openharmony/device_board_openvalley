354d48c6b7a017ad7b7a28e5d50214dbed378803
fangye <fangye@talkweb.com.cn>
2022-07-09 18:01:32 +0800

    更新版权头信息
    Signed-off-by: fangye <fangye@talkweb.com.cn>
    Change-Id: I862c54afd8a238ef916fee4ac505985ee7aaad01

--- niobeu4/liteos_m/include/ohos_run.h
+++ niobeu4/liteos_m/include/ohos_run.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2022 OpenValley Digital Co., Ltd.
+ * Copyright (c) 2022 Hunan OpenValley Digital Industry Development Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,13 +26,13 @@ extern "C" {
  * @brief 用与指定系统运行之前的程序入口函数
  *
  */
-#define BEFORE_OHOS_RUN(func) void *BEFORE_OHOS_RUN_FUNC_ENTRY = func
+#define BEFORE_OHOS_RUN(func) void *BEFORE_OHOS_RUN_FUNC_ENTRY = (func)
 
 /**
  * @brief 用与指定系统运行之后的程序入口函数
  *
  */
-#define OHOS_APP_RUN(func) void *OHOS_APP_FUNC_ENTRY = func
+#define OHOS_APP_RUN(func) void *OHOS_APP_FUNC_ENTRY = (func)
 
 #ifdef __cplusplus
 }
